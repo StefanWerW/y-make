b4e3812162eb68b2df56271971fd59afe41a7a98
StefanW <scaw.dev@gmail.com>
2015-07-24 03:33:26 +0200

    t

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var test = require('./vid-pipbuilder');
 var path = require('path');
 
 var VIDEOS_FOLDER = "videos";
-var vid_url = "https://www.youtube.com/watch?v=SSctqjzFjJ8;"
+var vid_url = "https://www.youtube.com/watch?v=U6IggHJTukg";
 var vid_path = path.join(__dirname, VIDEOS_FOLDER, 'dlTest.mp4');
 
 var info = {url: vid_url, vid_path: vid_path};

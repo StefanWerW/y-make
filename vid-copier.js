0a5a386efda097a491023e7f008644a81d9de820
StefanW <scaw.dev@gmail.com>
2015-07-24 03:22:29 +0200

    test

--- vid-copier.js
+++ vid-copier.js
@@ -14,10 +14,13 @@ exports.start = function(info){
   var vid_id = (id_counter++) + '.mp4';
   var vid_path = path.join(__dirname, VIDEOS_FOLDER, vid_id);
   info.vid_path = vid_path;
+  //download video
   downloader.download(info, function(){console.log('error');}, function(){
     console.log('DOWN SUCCESS');
+    //upload video
     uploader.upload(info, function(){console.log('UPLOAD ERROR CB')}, function(){
       console.log('UPLOAD SUCCESS');
+      //delete video
       fs.unlink(vid_path);
     });
   });
